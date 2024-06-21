4a012059d10e23d3861263b79563f4572f7bfdc7
johnkenzzy <johnkenumeh@gmail.com>
2024-06-20 21:29:11 -0700

    Created C programs that prints texts using puts and printf respectively

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * A program that uses the puts function to print text
+ *
+ * main - function is the enpoint
+ *
+ * puts -  function prints the character string on the screen
+ *
+ * return: value is set at zero (success)
+ */
+
 char main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;	
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ * A program that uses the puts function to print text
+ *
+ * main - function is the enpoint
+ *
+ * printf -  function prints the character string on the screen
+ *
+ * return: value is set at zero (success)
+ */
+
 char main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;	
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
