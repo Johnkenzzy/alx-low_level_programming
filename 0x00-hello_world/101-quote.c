cde75fa46d1abe21661c5d2051768c9d07307b68
johnkenzzy <johnkenumeh@gmail.com>
2024-06-20 23:53:13 -0700

    Prints texts with the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,11 @@
  * set to print text with write()*/int main(void)
 {
 	/*initialized message with the string value*/
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message1[] = "and that piece of art is useful\" - Dora Korpar, ";
+	const char message2[] = "2015-10-19\n";
 
-	write(2, message, 59);
+	write(2, message1, sizeof(message1) - 1);
+	write(2, message2, sizeof(message2) - 1);
 	/*return: value is set to one (error)*/
 	return (1);
 }
