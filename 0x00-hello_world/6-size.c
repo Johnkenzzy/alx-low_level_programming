fb433f3997492cc21b992e310af0d7daef21a394
johnkenzzy <johnkenumeh@gmail.com>
2024-06-20 21:54:38 -0700

    A C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
-    return 0;
+/**
+* A program that uses the puts function to print text
+*
+* main - function is the enpoint
+*
+* printf -  function prints the character string on the screen
+*
+* return: value is set at zero (success)
+*/
+
+int main(void) 
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
