88d7956c6c7eac0425f3ab1882ab652991a5f5c1
johnkenzzy <johnkenumeh@gmail.com>
2024-06-20 23:23:23 -0700

    Prints texts with the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <unistd.h>
 
-/**
-*
-* main - function is the enpoint
-*
-* write -  function prints the character string on the screen
-*
-* return: value is set at zero (success)
-*/
+/*A program that prints text without printf or puts, but write function*/
 
-int main(void)
+/**
+ * main - function is the enpoint
+ * set to print text with write()*/int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, message, 59);
+	/*return: value is set to one (error)*/
 	return (1);
 }
