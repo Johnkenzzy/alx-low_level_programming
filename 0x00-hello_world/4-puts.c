c5d0d67b204f757cbf555b06a40c2dea48c0de60
johnkenzzy <johnkenumeh@gmail.com>
2024-06-24 21:30:28 -0700

    Output printing with the put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,9 @@
  * main - function is the enpoint
  * return: value is set at zero (success)*/int main(void)
 {
+	char txt[] = "\"Programming is like building a multilingual puzzle";
+
 	/*puts -  function prints the character string on the screen*/
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts(txt);
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
+/* A program that uses the puts function to print text */
+
 /**
- * A program that uses the puts function to print text
- *
  * main - function is the enpoint
  *
  * printf -  function prints the character string on the screen
  *
  * return: value is set at zero (success)
- */
-
-int main(void)
+ */int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
