3b0e5d8e544cfb4f212d7707be4e6ee2e92236dd
johnkenzzy <johnkenumeh@gmail.com>
2024-06-25 22:29:34 -0700

    created a function that returns the absolute numbe rof an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*A function that computes the absolute value of an integer*/
+
+/**
+ * _abs() - prints absolute value
+ * takes an the integer as argument
+ *
+ * return an integer
+ */int _abs(int i)
+{
+	if (i < 1)
+	{
+		i = -i;
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
+	return (0);
+}
