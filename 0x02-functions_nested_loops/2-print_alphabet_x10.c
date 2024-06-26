27b951d5955461054dc8a01962249cebfc126fdc
johnkenzzy <johnkenumeh@gmail.com>
2024-06-25 19:27:54 -0700

    created a function that prints lowercase alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,14 @@
  *
  * returns nothing*/void print_alphabet_x10(void)
 {
-	char a = 'a';
-	char z = 'z';
+	char a;
+	char z;
 	int i = 0;
 
 	while (i < 10)
 	{
+		a = 'a';
+		z = 'z';
 		while (a <= z)
 		{
 			_putchar(a);
