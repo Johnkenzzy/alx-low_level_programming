76d9895d143b706b077289c3b615aaefd6604521
johnkenzzy <johnkenumeh@gmail.com>
2024-06-26 20:20:24 -0700

    Created a function that adds to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* A function that adds two integers and returns the result */
+
+	/**
+	* add function - give the addition of 2 numbers
+	* takes towo intergers as agument
+	*
+	* returns the sum two integers
+	*/int add(int a, int b)
+{
+	int result;
+
+	result= a + b;
+	_putchar('0' + result);
+	return (result);
+}
