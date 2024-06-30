64fa7da440176cf014227da7f29538e63ca9c44d
johnkenzzy <johnkenumeh@gmail.com>
2024-06-30 10:39:10 -0700

    Fixed the nested conditional to add a day to a leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,8 +8,7 @@
 	* @day: day of month
 	* @year: year
 	* Return: void
-	*/
-	void print_remaining_days(int month, int day, int year)
+	*/void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
