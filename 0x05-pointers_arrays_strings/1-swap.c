a8f6bfb192f0e3fd4f9889e55a06300387ffc570
johnkenzzy <johnkenumeh@gmail.com>
2024-07-02 06:05:55 -0700

    functions and header file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 /* A function that swaps the values of two integers */
 #include "main.h"
-/**
-* swap_int - swap integers values
-* @arg *a - the first integer
-* @arg *b - the second integer
-* returns nothing
-*/void swap_int(int *a, int *b)
+	/**
+	* swap_int - swap integers values
+	* @arg *a - the first integer
+	* @arg *b - the second integer
+	* returns nothing
+	*/void swap_int(int *a, int *b)
 {
 	*a = *b;
 	*b = *a;
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/* A function that returns the length of a string */
+#include "main.h"
+
+	/**
+	* _strlen - checks the length of a string
+	* @arg *s - the string array
+	* return - string length
+	*/int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
