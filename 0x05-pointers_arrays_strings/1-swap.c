3f4db71e191597fb2df632fab529c269797ee8a7
Johnkennedy Umeh <126101340+Johnkenzzy@users.noreply.github.com>
2024-07-02 03:46:57 +0100

    Create integer swapping function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/* A function that swaps the values of two integers */
+#include "main.h"
+/**
+* swap_int - swap integers values
+* @arg *a - the first integer
+* @arg *b - the second integer
+* returns nothing
+*/void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
