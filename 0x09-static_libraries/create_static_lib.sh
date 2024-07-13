72841ae1331fb34e235a798b38d41f9ee4529f4d
johnkenzzy <johnkenumeh@gmail.com>
2024-07-12 21:33:43 -0700

    fix

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c | ar -rc liball.a *.o
+
+# Create object files from all .c files in current directory
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
+
+# Create the static library from the object frles
+ar -rc liball.a *.o
+
+# Clean up object files
+rm *.o
