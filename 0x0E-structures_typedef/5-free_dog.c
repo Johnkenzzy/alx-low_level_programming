b77be77f6602e1a2618211603553a746a3fb1cb8
johnkenzzy <johnkenumeh@gmail.com>
2024-07-22 21:50:26 -0700

    free memory taken by new_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+	/**
+	* free_dog - frees dog
+	* @d: pointer to the dog to be freed
+	*/void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
