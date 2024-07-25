631b9135b20a29d9a176484c4254a9247fc2e0cb
johnkenzzy <johnkenumeh@gmail.com>
2024-07-24 19:21:58 -0700

    project set up and print_name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+	/**
+	* print_name - prints a name
+	* @name: pointer to a name
+	* @(*f)(char *): a function pointer,
+	* takes a character pointer as an argument
+	* return: nothing
+	*/void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,7 @@
+# C - Function Pointers
+
+## To understand:
+
+* Function pointers and how to use them
+* What a function pointer exactly hold
+* Where a function pointer point to in the virtual memory
