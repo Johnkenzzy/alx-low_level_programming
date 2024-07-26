c14ca514808c9fbb46435f67f8caba5b9671c687
johnkenzzy <johnkenumeh@gmail.com>
2024-07-25 19:39:10 -0700

    directory creation and wrote a variadic function that sums numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+
+	/**
+	* sum_them_all - sum up all its paramters
+	* @n: input count
+	* @...: argument list
+	* return: the sum of all parameters
+	*/int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+	unsigned int sum;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	sum = 0;
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(nums, unsigned int);
+	}
+	va_end(nums);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+## C - Variadic Functions
+
+### To understand:
+
+* What are variadic functions
+* What are variadic functions
+* Why and how to use the const type qualifier.
