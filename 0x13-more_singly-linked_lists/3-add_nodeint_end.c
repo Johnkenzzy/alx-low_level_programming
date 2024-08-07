495b05e8de50ae6ac898266cb5997ce29395cbfa
johnkenzzy <johnkenumeh@gmail.com>
2024-08-06 20:47:33 -0700

    betty styling

--- 0x13-more_singly-linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly-linked_lists/3-add_nodeint_end.c
@@ -10,8 +10,8 @@
 	*/listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *temp;
-
 	listint_t *new_node = malloc(sizeof(listint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
