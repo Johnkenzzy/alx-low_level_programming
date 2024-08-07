4a51607b690b1d353f11f30773c59d74abf500cc
johnkenzzy <johnkenumeh@gmail.com>
2024-08-07 06:04:00 -0700

    fixed task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,15 @@
 
 	for (num = 0; num < 10; num++)
 	{
+		putchar(num + '0');
 		if (num != 9)
 		{
-			putchar(num + '0');
 			putchar(',');
+			putchar(' ');
 		}
-		else
-			putchar(num + '0');
 
-		putchar(' ');
 	}
+	putchar('\n');
 
 	return (0);
 }
