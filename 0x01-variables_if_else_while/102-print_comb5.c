a234d1e40437efbbdbd2199d33465e830e5bdaeb
johnkenzzy <johnkenumeh@gmail.com>
2024-08-11 07:21:06 -0700

    fixed with appropriate requirements

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,65 +1,37 @@
-/* Prints all possible different combinations of three digits */
 #include <stdio.h>
 
-void print_integer(int n);
+/* Prints all possible different combinations of three digits */
 
 /*
- * main - prints possible unique combination pairs
- * starts from zero
- * return: 0 Always successful */int main(void)
+ * prints possible unique combinations
+ * starts from zero */int main(void)
 {
-	int number1;
-	int number2;
+	int n1;
+	int n2;
+	int n3;
+	int n4;
 
-	/* loop through numbers */
-	for (number1 = 0; number1 < 100; number1++)
+	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (number2 = number1 + 1; number2 < 100; number2++)
+		for (n2 = 0; n2 < 10; n2++)
 		{
-			if (number1 < 10)
-				putchar(0 + '0');
-
-			print_integer(number1);
-			putchar(' ');
-
-			if (number2 < 10)
-				putchar(0 + '0');
-
-			print_integer(number2);
-
-			if (number1 != 98 && number2 < 100)
+			for (n3 = n1; n3 < 10; n3++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (n4 = n2 + 1; n4 < 10; n4++)
+				{
+					putchar(n1 + '0');
+					putchar(n2 + '0');
+					putchar(' ');
+					putchar(n3 + '0');
+					putchar(n4 + '0');
+					if (n1 + n2 != 17)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
 	}
 	putchar('\n');
-	return (0);
 }
-
-	/**
-	* print_int - prints an integer
-	* @n: integer argument
-	* return: nothing
-	*/void print_integer(int n)
-{
-
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-
-	if (n / 10 != 0)
-		print_integer(n / 10);
-
-	putchar((n % 10) + '0');
-}
-
