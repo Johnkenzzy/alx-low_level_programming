5ea1208f94c73178b8e1fe15f94b9c16cc4d0b07
johnkenzzy <johnkenumeh@gmail.com>
2024-08-13 09:51:21 -0700

    advanced task 2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+	/**
+	* main - sums up the number below 1024, multiple of 3 or 5
+	* return: nothing
+	*/void main(void)
+{
+	int i;
+	int mul = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			mul = mul + i;
+		}
+	}
+	printf("%d\n", mul);
+}
