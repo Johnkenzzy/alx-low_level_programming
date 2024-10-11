7c872d784acaa4e1416999839ed81c83822c854d
johnkenzzy <johnkenumeh@gmail.com>
2024-10-11 08:27:10 -0700

    Error handling

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
 	unsigned long int i;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
 	table->size = size;
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * table->size);
-	if ((table->array) == NULL)
+	if (!(table->array))
+	{
+		free(table);
+		return (NULL);
+	}
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
