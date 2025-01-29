752e9f41eb2722eb2a2c791d9adefed02b85a5f3
johnkenzzy <johnkenumeh@gmail.com>
2025-01-29 06:10:17 -0800

    Fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,16 +12,15 @@
 int linear_search(int *array, size_t size, int  value)
 {
 	size_t i;
-	int j = 0;
 
 	if (!array)
 		return (-1);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		j++;
 		if (array[i] == value)
-			return (j);
+			return ((int)i);
 	}
 
 	return (-1);
